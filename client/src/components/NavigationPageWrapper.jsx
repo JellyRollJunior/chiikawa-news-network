969765ad5ad10d2334d0e5ae3676fc901116e361
Brandon Lin <brandonlin9716@gmail.com>
2025-08-19 17:43:44 -0700

    Show nav labels on mobile

--- client/src/components/NavigationPageWrapper.jsx
+++ client/src/components/NavigationPageWrapper.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router';
+import { Link } from 'react-router';
 import logo from '../assets/nav/chiikawa-glasses.png';
 import home from '../assets/nav/chiikawa-book.png';
 import messages from '../assets/nav/chii-hachi-hearts.png';
@@ -10,60 +10,53 @@ const NavButton = ({ link, label, src, srcWidth = '2.75rem', style }) => {
   return (
     <button
       style={style}
-      className="mx-auto flex w-fit justify-center rounded-2xl px-1 py-1 hover:bg-gray-400"
+      className="flex h-full w-full items-end justify-center pb-1 hover:bg-gray-400"
     >
       <Link
         className="flex flex-col items-center justify-center lg:flex-row"
         to={link}
       >
         <img style={{ width: srcWidth }} src={src} />
-        <h3 className="text-shadow-wrap hidden font-bold md:block">{label}</h3>
+        <h3 className="text-shadow-wrap font-bold">{label}</h3>
       </Link>
     </button>
   );
 };
 
 const NavigationPageWrapper = ({ children }) => {
-  const location = useLocation();
-  const path = location.pathname;
-
   return (
     <>
-      <nav className="md:w-18 h-18 fixed bottom-0 grid w-full grid-cols-6 items-center justify-center bg-gray-300 md:top-0 md:flex md:h-full md:flex-col md:justify-start md:gap-3 md:pt-8">
-        <NavButton link="/" label="" src={logo} />
-        <NavButton
-          link="/"
-          label="home"
-          src={home}
-          srcWidth="2.75rem"
-          style={{ marginTop: '3.5rem' }}
-        />
+      <nav className="md:w-18 fixed bottom-0 grid h-20 w-full grid-cols-5 bg-gray-300 md:top-0 md:flex md:h-full md:flex-col md:justify-start md:gap-3">
+        <div className="hidden h-full w-full md:block">
+          <NavButton link="/" label="CNN" src={logo} />
+        </div>
+        <NavButton link="/" label="Home" src={home} srcWidth="2.5rem" />
         <NavButton
           link="/chats"
-          label="chats"
+          label="Chats"
           src={messages}
           srcWidth="3.75rem"
         />
         <NavButton
           link="/profile"
-          label="profile"
+          label="Profile"
           src={profile}
           srcWidth="3.5rem"
         />
         <NavButton
           link="/profile"
-          label="users"
+          label="Users"
           src={users}
           srcWidth="3.5rem"
         />
         <NavButton
           link="/profile"
-          label="settings"
+          label="Settings"
           src={settings}
-          srcWidth="2.75rem"
+          srcWidth="2.2rem"
         />
       </nav>
-      <div className="md:pl-18 h-screen pb-12 md:pb-0">{children}</div>
+      <div className="md:pl-18 h-screen pb-20 md:pb-0">{children}</div>
     </>
   );
 };
