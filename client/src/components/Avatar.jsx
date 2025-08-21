a22a4a96241db20c8911fdac568687f2922a69ab
Brandon Lin <brandonlin9716@gmail.com>
2025-08-21 14:37:40 -0700

    Style avatar circles on chat page

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 
-const Avatar = ({ avatar, size = 4 }) => {
+const Avatar = ({ avatar, size = 4, chatStyling = false }) => {
   const [error, setError] = useState(false);
 
   return (
     <div
       style={{ height: `${size}rem`, width: `${size}rem` }}
-      className="border-1 shrink-0 overflow-clip rounded-full border-amber-900 bg-pink-50"
+      className={`border-1 shrink-0 overflow-clip rounded-full border-amber-900 bg-pink-50 ${chatStyling ? 'block-shadow border-yellow-300 bg-yellow-50' : ''}`}
     >
       {!error && avatar && (
         <img
