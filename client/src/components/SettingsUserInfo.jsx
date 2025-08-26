518b2c731b0022db8f05b9cc4d9d3016df62e68d
Brandon Lin <brandonlin9716@gmail.com>
2025-08-26 10:19:21 -0700

    Extract SettingsUserInfo loading element

--- client/src/components/SettingsUserInfo.jsx
+++ client/src/components/SettingsUserInfo.jsx
@@ -5,6 +5,62 @@ import { useEditBio } from '../hooks/useEditBio.js';
 // eslint-disable-next-line no-unused-vars
 import { motion } from 'motion/react';
 
+const LoadingElement = () => {
+  return (
+    <>
+      <section className="yellow-block mt-2 flex items-center px-3 py-2">
+        <motion.div
+          className="size-12 rounded-full"
+          style={{ backgroundColor: '#fff7ed' }}
+          animate={{ backgroundColor: '#ccfbf1' }}
+          transition={{
+            repeat: Infinity,
+            repeatType: 'reverse',
+            duration: 0.8,
+            ease: 'easeInOut',
+          }}
+        />
+        <motion.div
+          className="ml-3 h-5 w-16 rounded-full"
+          style={{ backgroundColor: '#fff7ed' }}
+          animate={{ backgroundColor: '#ccfbf1' }}
+          transition={{
+            repeat: Infinity,
+            repeatType: 'reverse',
+            duration: 0.8,
+            ease: 'easeInOut',
+          }}
+        />
+        <button className="blue-button ml-auto px-3 py-1" disabled={true}>
+          Change Photo
+        </button>
+      </section>
+      <section className="yellow-block mt-2 px-3 py-2">
+        <form className="flex flex-col">
+          <h3 className="mt-2 font-medium">Bio</h3>
+          <motion.p
+            className="mt-2 h-32 w-full rounded-sm text-xl font-bold"
+            style={{ backgroundColor: '#fff7ed' }}
+            animate={{ backgroundColor: '#ccfbf1' }}
+            transition={{
+              repeat: Infinity,
+              repeatType: 'reverse',
+              duration: 0.8,
+              ease: 'easeInOut',
+            }}
+          />
+          <button
+            className="blue-button ml-auto mt-2 px-3 py-1"
+            disabled={true}
+          >
+            Confirm Edit
+          </button>
+        </form>
+      </section>
+    </>
+  );
+};
+
 const SettingsUserInfo = () => {
   const { editBio, isLoading: isEditingBio } = useEditBio();
   const { id, bio, setBio, username, avatar, isLoading } =
@@ -29,57 +85,7 @@ const SettingsUserInfo = () => {
         <h2 className="font-chiikawa py-2 text-center text-lg">Edit Profile</h2>
       </header>
       {isLoading ? (
-        <>
-          <section className="yellow-block mt-2 flex items-center px-3 py-2">
-            <motion.div
-              className="size-12 rounded-full"
-              style={{ backgroundColor: '#fff7ed' }}
-              animate={{ backgroundColor: '#ccfbf1' }}
-              transition={{
-                repeat: Infinity,
-                repeatType: 'reverse',
-                duration: 0.8,
-                ease: 'easeInOut',
-              }}
-            />
-            <motion.div
-              className="ml-3 h-5 w-16 rounded-full"
-              style={{ backgroundColor: '#fff7ed' }}
-              animate={{ backgroundColor: '#ccfbf1' }}
-              transition={{
-                repeat: Infinity,
-                repeatType: 'reverse',
-                duration: 0.8,
-                ease: 'easeInOut',
-              }}
-            />
-            <button className="blue-button ml-auto px-3 py-1" disabled={true}>
-              Change Photo
-            </button>
-          </section>
-          <section className="yellow-block mt-2 px-3 py-2">
-            <form className="flex flex-col" onSubmit={handleEditBio}>
-              <h3 className="mt-2 font-medium">Bio</h3>
-              <motion.p
-                className="mt-2 h-32 w-full rounded-sm text-xl font-bold"
-                style={{ backgroundColor: '#fff7ed' }}
-                animate={{ backgroundColor: '#ccfbf1' }}
-                transition={{
-                  repeat: Infinity,
-                  repeatType: 'reverse',
-                  duration: 0.8,
-                  ease: 'easeInOut',
-                }}
-              />
-              <button
-                className="blue-button ml-auto mt-2 px-3 py-1"
-                disabled={true}
-              >
-                Confirm Edit
-              </button>
-            </form>
-          </section>
-        </>
+        <LoadingElement />
       ) : (
         <>
           <section className="yellow-block mt-2 flex items-center px-3 py-2">
