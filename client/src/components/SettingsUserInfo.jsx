b475cf8124566a16dc89f13625c9b762891ec36f
Brandon Lin <brandonlin9716@gmail.com>
2025-08-28 19:25:01 -0700

    Update SettingsUserInfo loading for new UI. Refactor return statement
    Return statement shows main element HTML first then loading elem HTML

--- client/src/components/SettingsUserInfo.jsx
+++ client/src/components/SettingsUserInfo.jsx
@@ -3,35 +3,9 @@ import { CurrentContext } from '../contexts/CurrentProvider.jsx';
 import { useUploadAvatar } from '../hooks/useUploadAvatar.js';
 import { useEditBio } from '../hooks/useEditBio.js';
 import { Avatar } from './Avatar.jsx';
+import { LoadingElement } from './LoadingElement.jsx';
 import chiiPeace from '../assets/images/chii-peace.png';
 import hachiCamera from '../assets/images/hachi-camera-back.png';
-import { LoadingElement } from './LoadingElement.jsx';
-
-const LoadingElements = () => {
-  return (
-    <>
-      <section className="yellow-block mt-2 flex items-center px-3 py-2">
-        <LoadingElement className='size-12 rounded-full' />
-        <LoadingElement className='ml-3 h-5 w-16 rounded-full' />
-        <button className="blue-button ml-auto px-3 py-1" disabled={true}>
-          Change Photo
-        </button>
-      </section>
-      <section className="yellow-block mt-2 px-3 py-2">
-        <form className="flex flex-col">
-          <h3 className="mt-2 font-medium">Bio</h3>
-          <LoadingElement className='mt-2 h-32 w-full rounded-sm' />
-          <button
-            className="blue-button ml-auto mt-2 px-3 py-1"
-            disabled={true}
-          >
-            Confirm Edit
-          </button>
-        </form>
-      </section>
-    </>
-  );
-};
 
 const SettingsUserInfo = () => {
   const { id, bio, setBio, avatar, isLoading } = useContext(CurrentContext);
@@ -67,57 +41,77 @@ const SettingsUserInfo = () => {
     }
   };
 
-  return (
+  return !isLoading ? (
     <>
-      {isLoading ? (
-        <LoadingElements />
-      ) : (
-        <>
-          <section className="mt-3 yellow-block flex flex-col items-center py-2">
-              <div className=" flex items-center px-[16px] relative">
-                <Avatar avatar={avatar ? avatar : null} size={6} />
-                <img className="w-[52px] drop-shadow-pink-outline absolute -top-[10px] -right-[24px]" src={chiiPeace} />
-                <img className="w-[52px] drop-shadow-pink-outline absolute -bottom-[6px] -left-[24px]" src={hachiCamera} />
-              </div>
-                <button
-                  className="blue-button px-3 py-1 mt-3"
-                  onClick={handleClickChangePhoto}
-                  disabled={isUploadingAvatar}
-                >
-                  Change Picture
-                </button>
-                <input
-                  type="file"
-                  ref={fileInputRef}
-                  className="hidden"
-                  onChange={handleUploadAvatar}
-                  accept="image/jpg, image/jpeg, image/png, image/gif, image/webp"
-                />
-          </section>
-          <section className="yellow-block mt-3 px-3 py-2">
-            <form className="flex flex-col" onSubmit={handleEditBio}>
-              <h3 className="mt-2 font-medium">Bio</h3>
-              <textarea
-                className="border-3 mt-2 min-h-32 w-full rounded-md border-pink-200 px-1 focus:outline-2 focus:outline-green-300"
-                autoFocus
-                name="bio"
-                id="bio"
-                value={bioTextarea}
-                onChange={(event) => setBioTextarea(event.target.value)}
-                minLength={1}
-                maxLength={350}
-                required
-              />
-              <button
-                className="blue-button ml-auto mt-2 px-3 py-1"
-                disabled={isEditingBio}
-              >
-                Confirm Edit
-              </button>
-            </form>
-          </section>
-        </>
-      )}
+      <section className="yellow-block mt-3 flex flex-col items-center py-2">
+        <div className="relative flex items-center px-[16px]">
+          <Avatar avatar={avatar ? avatar : null} size={6} />
+          <img className="w-[52px] drop-shadow-pink-outline absolute -top-[10px] -right-[24px]" src={chiiPeace} />
+          <img className="w-[52px] drop-shadow-pink-outline absolute -bottom-[6px] -left-[24px]" src={hachiCamera} />
+        </div>
+        <button
+          className="blue-button mt-3 px-3 py-1"
+          onClick={handleClickChangePhoto}
+          disabled={isUploadingAvatar}
+        >
+          Change Picture
+        </button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          className="hidden"
+          onChange={handleUploadAvatar}
+          accept="image/jpg, image/jpeg, image/png, image/gif, image/webp"
+        />
+      </section>
+      <section className="yellow-block mt-3 px-3 py-2">
+        <form className="flex flex-col" onSubmit={handleEditBio}>
+          <h3 className="mt-2 font-medium">Bio</h3>
+          <textarea
+            className="border-3 mt-2 min-h-32 w-full rounded-md border-pink-200 px-1 focus:outline-2 focus:outline-green-300"
+            autoFocus
+            name="bio"
+            id="bio"
+            value={bioTextarea}
+            onChange={(event) => setBioTextarea(event.target.value)}
+            minLength={1}
+            maxLength={350}
+            required
+          />
+          <button
+            className="blue-button ml-auto mt-2 px-3 py-1"
+            disabled={isEditingBio}
+          >
+            Confirm Edit
+          </button>
+        </form>
+      </section>
+    </>
+  ) : (
+    /* Loading Display */
+    <>
+      <section className="yellow-block mt-3 flex flex-col items-center py-2">
+        <div className="relative flex items-center px-[16px]">
+          <LoadingElement className="size-24 rounded-full" />
+          <img className="w-[52px] drop-shadow-pink-outline absolute -top-[10px] -right-[24px]" src={chiiPeace} />
+          <img className="w-[52px] drop-shadow-pink-outline absolute -bottom-[6px] -left-[24px]" src={hachiCamera} />
+        </div>
+        <button className="blue-button mt-3 px-3 py-1" disabled={true}>
+          Change Picture
+        </button>
+      </section>
+      <section className="yellow-block mt-2 px-3 py-2">
+        <form className="flex flex-col">
+          <h3 className="mt-2 font-medium">Bio</h3>
+          <LoadingElement className="mt-2 h-32 w-full rounded-sm" />
+          <button
+            className="blue-button ml-auto mt-2 px-3 py-1"
+            disabled={true}
+          >
+            Confirm Edit
+          </button>
+        </form>
+      </section>
     </>
   );
 };
