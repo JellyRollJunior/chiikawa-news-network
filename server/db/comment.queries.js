9b1a44998ad2604b7d5900147cfe840d6e7cd95b
Brandon Lin <brandonlin9716@gmail.com>
2025-09-01 21:18:32 -0700

    Implement query to create comment

---
+++ server/db/comment.queries.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+import { DatabaseError } from '../errors/DatabaseError.js';
+
+const prisma = new PrismaClient();
+
+const createComment = async (userId, postId, content, media = null) => {
+    try {
+        const data = prisma.comment.create({
+            data: {
+                content,
+                media,
+                post: {
+                    connect: {
+                        id: postId,
+                    },
+                },
+                author: {
+                    connect: {
+                        id: userId,
+                    },
+                },
+            },
+        });
+        return data;
+    } catch (error) {
+        throw new DatabaseError('Unable to create comment');
+    }
+};
+
+export { createComment };
