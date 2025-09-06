521d97fd512d0f7ac52ce011205df29128bbb21c
Brandon Lin <brandonlin9716@gmail.com>
2025-09-05 17:04:32 -0700

    Extract resizing and converting image logic

--- server/middleware/Sharp.js
+++ server/middleware/Sharp.js
@@ -1,18 +1,31 @@
 import sharp from 'sharp';
 
+const resizeImage = async (file, width) => {
+    const animated = file.mimetype.split('/')[1] == 'gif' ? true : false;
+    const resizedBuffer = await sharp(file.buffer, { animated })
+        .resize(width, null)
+        .toBuffer();
+    return resizedBuffer;
+};
+
+const convertToWebp = async (file, quality) => {
+    const animated = file.mimetype.split('/')[1] == 'gif' ? true : false;
+    const convertedBuffer = await sharp(file.buffer, { animated })
+        .webp({ quality })
+        .toBuffer();
+    return convertedBuffer;
+};
+
 const resizeAvatar = async (req, res, next) => {
     if (!req.file || !req.file.buffer) next();
     try {
-        const animated =
-            req.file.mimetype.split('/')[1] == 'gif' ? true : false;
-        const resizedBuffer = await sharp(req.file.buffer, { animated })
-            .resize(200, null)
-            .webp({ quality: 75 })
-            .toBuffer();
-        req.file.buffer = resizedBuffer;
-        req.file.mimetype = 'image/webp'
+        req.file.buffer = await resizeImage(req.file, 200);
+        req.file.buffer = await convertToWebp(req.file, 75);
+        req.file.mimetype = 'image/webp';
         next();
-    } catch (error) {}
+    } catch (error) {
+        next(error);
+    }
 };
 
 export { resizeAvatar };
