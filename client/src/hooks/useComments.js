ffdbb7e3c04cddeb52823afe16029035302fa1a0
Brandon Lin <brandonlin9716@gmail.com>
2025-09-10 12:27:11 -0700

    Implement toggle like on comments

--- client/src/hooks/useComments.js
+++ client/src/hooks/useComments.js
@@ -1,11 +1,16 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { useTokenErrorHandler } from './useTokenErrorHandler.js';
 import { ToastContext } from '../contexts/ToastProvider.jsx';
-import { fetchComments } from '../services/postApi.js';
+import {
+    createCommentLike,
+    deleteCommentLike,
+    fetchComments,
+} from '../services/postApi.js';
 
 const useComments = (postId) => {
     const [comments, setComments] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [isLoadingLike, setIsLoadingLike] = useState(false);
     const { handleTokenErrors } = useTokenErrorHandler();
     const { toast } = useContext(ToastContext);
 
@@ -35,7 +40,39 @@ const useComments = (postId) => {
         return () => abortController.abort();
     }, [postId, getComments]);
 
-    return { comments, isLoading };
+    let likeAbortController = new AbortController();
+    const toggleLike = async (commentId, hasLiked = false) => {
+        try {
+            setIsLoadingLike(true);
+            if (likeAbortController) likeAbortController.abort();
+            likeAbortController = new AbortController();
+            const updatedComment = !hasLiked
+                ? await createCommentLike(likeAbortController.signal, commentId)
+                : await deleteCommentLike(
+                      likeAbortController.signal,
+                      commentId
+                  );
+            // update comment arry with new data
+            setComments((prevComments) => {
+                const index = prevComments.findIndex(
+                    (comment) => comment.id == commentId
+                );
+                if (index >= 0) {
+                    const updatedComments = [...prevComments];
+                    updatedComment[index] = updatedComment;
+                    return updatedComments;
+                }
+                return prevComments;
+            });
+        } catch (error) {
+            handleTokenErrors(error);
+            toast('Unable to like/unlike comment');
+        } finally {
+            setIsLoadingLike(false);
+        }
+    };
+
+    return { comments, isLoading, toggleLike, isLoadingLike };
 };
 
 export { useComments };
