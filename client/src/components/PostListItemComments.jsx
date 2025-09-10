d7e16df83327cb8730e81fce8aea56ba99dc1f51
Brandon Lin <brandonlin9716@gmail.com>
2025-09-10 11:40:41 -0700

    Add message input to comments section

--- client/src/components/PostListItemComments.jsx
+++ client/src/components/PostListItemComments.jsx
@@ -1,34 +1,52 @@
 import { useComments } from '../hooks/useComments.js';
 import { Avatar } from './Avatar.jsx';
 import { IncrementButton } from './IncrementButton.jsx';
+import { useContext } from 'react';
+import { CurrentContext } from '../contexts/CurrentProvider.jsx';
 import heart from '../assets/svgs/heart.svg';
 import heartFilled from '../assets/svgs/heart-filled.svg';
+import send from '../assets/svgs/send.svg';
 
 const PostListItemComments = ({ postId }) => {
+  const { avatar } = useContext(CurrentContext);
   const { comments, isLoading } = useComments(postId);
 
   return (
     <>
-    <hr className='w-full h-1 border-yellow-300 mt-4.5' />
-    <ul className="mt-3 flex flex-col gap-3">
-      {comments &&
-        comments.map((comment) => (
-          <li className="flex text-sm" key={comment.id}>
-            <Avatar size={2} avatar={comment.author && comment.author.avatar} />
-            <div className="ml-2 flex-1">
-              <h4 className="font-bold">
-                {comment.author && comment.author.username}
-              </h4>
-              <p>{comment.content}</p>
-            </div>
-            <IncrementButton
-              className='mt-0.5 flex-col ml-1 mr-0.5 '
-              src={comment.hasLiked ? heartFilled : heart}
-              count={comment.likeCount}
-            />
-          </li>
-        ))}
-    </ul>
+      <hr className="mt-3 h-1 w-full border-yellow-300" />
+      <ul className="mt-3 flex flex-col gap-4">
+        {comments &&
+          comments.map((comment) => (
+            <li className="flex text-sm" key={comment.id}>
+              <Avatar
+                size={2}
+                avatar={comment.author && comment.author.avatar}
+              />
+              <div className="ml-2 flex-1">
+                <h4 className="font-bold">
+                  {comment.author && comment.author.username}
+                </h4>
+                <p>{comment.content}</p>
+              </div>
+              <IncrementButton
+                className="mt-0.5 mr-0.5 ml-1 flex-col"
+                src={comment.hasLiked ? heartFilled : heart}
+                count={comment.likeCount}
+              />
+            </li>
+          ))}
+      </ul>
+      <form className="mt-3 flex">
+        <Avatar size={2} avatar={avatar} />
+        <textarea
+          className="ml-2 flex-1 rounded-lg border-1 border-pink-200 bg-white py-1 pl-2"
+          type="text"
+          placeholder="Share your thoughts..."
+        />
+        <button className="ml-2">
+          <img className="w-5" src={send} />
+        </button>
+      </form>
     </>
   );
 };
