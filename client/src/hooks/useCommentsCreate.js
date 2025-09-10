a6d8d6fb965432be24b3b146f0b5553bfe1350cc
Brandon Lin <brandonlin9716@gmail.com>
2025-09-10 14:52:28 -0700

    Implement hook for creating comment

--- client/src/hooks/useCommentsCreate.js
+++ client/src/hooks/useCommentsCreate.js
@@ -1,11 +1,33 @@
-import { useContext, useState } from "react";
-import { useTokenErrorHandler } from "./useTokenErrorHandler.js";
-import { ToastContext } from "../contexts/ToastProvider.jsx";
+import { useContext, useState } from 'react';
+import { useTokenErrorHandler } from './useTokenErrorHandler.js';
+import { ToastContext } from '../contexts/ToastProvider.jsx';
+import { createComment } from '../services/postApi.js';
 
-const useCommentsCreate = (postId) => {
+const useCommentsCreate = () => {
     const [isLoading, setIsLoading] = useState(false);
     const { handleTokenErrors } = useTokenErrorHandler();
     const { toast } = useContext(ToastContext);
+
+    let abortController = new AbortController();
+    const postComment = async (postId, content) => {
+        if (abortController) abortController.abort();
+        abortController = new AbortController();
+        try {
+            setIsLoading(true);
+            await createComment(
+                abortController.signal,
+                postId,
+                content
+            );
+        } catch (error) {
+            handleTokenErrors(error);
+            toast('Unable to post comment');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return { postComment, isLoading };
 };
 
 export { useCommentsCreate };
