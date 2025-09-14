bd1816594735d2154f0714a4c75871ac2a97623f
Brandon Lin <brandonlin9716@gmail.com>
2025-09-14 12:32:21 -0700

    Add refresh button to userPosts

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -2,6 +2,7 @@ import { usePosts } from '../hooks/usePosts.js';
 import { UserInfo } from './UserInfo.jsx';
 import { Scrollable } from './Scrollable.jsx';
 import { PostList } from './PostsList.jsx';
+import { RefreshButton } from './RefreshButton.jsx';
 import logo from '../assets/nav/chiikawa-glasses.png';
 
 const UserProfile = ({ userId }) => {
@@ -21,7 +22,7 @@ const UserProfile = ({ userId }) => {
       <Scrollable onScrollToBottom={fetchNextPage}>
         <UserInfo userId={userId} />
         <section className="mx-2 mt-1 mb-2 flex flex-1 flex-col overflow-hidden">
-          <div className="my-2 flex items-center justify-center gap-2">
+          <div className="my-2 flex items-center justify-center gap-4">
             <img
               className="w-[43px]"
               src={logo}
@@ -30,7 +31,11 @@ const UserProfile = ({ userId }) => {
             <h2 className="font-chiikawa text-shadow-wrap text-center text-2xl font-bold">
               Posts
             </h2>
+            <div className="mt-2">
+              <RefreshButton onclick={refreshPosts} />
+            </div>
           </div>
+
           <div className="mt-1 flex-1 overflow-y-scroll">
             <PostList
               posts={posts}
