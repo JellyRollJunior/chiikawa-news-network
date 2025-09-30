a0a608fa8bc2a52fd3657794683e9a78835488b3
Brandon Lin <brandonlin9716@gmail.com>
2025-09-30 15:05:26 -0700

    Make HomeUsersAside scroll when overflow

--- client/src/components/HomeUsersAside.jsx
+++ client/src/components/HomeUsersAside.jsx
@@ -6,22 +6,24 @@ const HomeUsersAside = () => {
   const { users, isLoading } = useUsers();
 
   return (
-    <aside className="main-container mx-4 mt-3 mb-2 hidden w-xs flex-col gap-2 px-3 pt-3.5 pb-2.5 md:flex">
-      <header className="flex items-center justify-center gap-2">
+    <aside className="main-container scrollbar-thin mx-4 mt-3 mb-2 hidden w-xs flex-col gap-2 overflow-y-scroll px-3 pt-3.5 pb-2.5 md:flex">
+      <header className="flex shrink-0 items-center justify-center gap-2">
         <div className="yellow-block flex-1 pt-1 pb-0.5">
-          <h2 className="font-chiikawa text-shadow-wrap px-2 pt-2 pb-1 text-center ">
+          <h2 className="font-chiikawa text-shadow-wrap px-2 pt-2 pb-1 text-center">
             People you may know
           </h2>
         </div>
       </header>
-      <div className="duckegg-block h-4" />
+      <div className="duckegg-block h-4 shrink-0" />
       <ul className="flex flex-1 flex-col gap-2">
         {users &&
           users.map((user, index) => (
-            <li className="flex gap-2">
-              <div className={`${BLOCKS[index % BLOCKS.length]} px-2 py-2`}>
+            <li key={user.id} className="flex gap-2">
+              <div
+                className={`${BLOCKS[index % BLOCKS.length]} px-2 pt-2 pb-1.5`}
+              >
                 <Avatar
-                  className="size-[42px] border-1 border-yellow-500"
+                  className="size-[52px] border-1 border-yellow-500"
                   avatar={user.avatar}
                   size={1.75}
                   secondaryStyling={true}
@@ -35,7 +37,7 @@ const HomeUsersAside = () => {
             </li>
           ))}
       </ul>
-      <div className="duckegg-block h-4" />
+      <div className="duckegg-block h-4 shrink-0" />
     </aside>
   );
 };
