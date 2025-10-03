3b3a0b8d7d88b2bb1aa2591e7c51250ce12eea6e
Brandon Lin <brandonlin9716@gmail.com>
2025-10-03 14:33:02 -0700

    Implement overlay scroll on desktop HomeUsersAside

--- client/src/components/HomeUsersAside.jsx
+++ client/src/components/HomeUsersAside.jsx
@@ -1,3 +1,4 @@
+import SimpleBar from 'simplebar-react';
 import { Fragment, useContext } from 'react';
 import { useUsers } from '../hooks/useUsers.js';
 import { CurrentContext } from '../contexts/CurrentProvider.jsx';
@@ -12,36 +13,38 @@ const HomeUsersAside = () => {
     : [];
 
   return (
-    <aside className="main-container scrollbar-thin mx-4 mt-3 mb-2 hidden md:w-3xs lg:w-xs flex-col gap-2 overflow-y-scroll px-3 pt-3.5 pb-2.5 md:flex">
-      <header className="flex shrink-0 items-center justify-center gap-2">
-        <div className="yellow-block flex-1 pt-1 pb-0.5">
-          <h2 className="font-chiikawa text-shadow-wrap px-2 pt-2 pb-1 text-center">
-            People you may know
-          </h2>
-        </div>
-      </header>
-      <div className="duckegg-block h-4 shrink-0" />
-      <ul className="flex flex-1 flex-col gap-2">
-        {!isLoading
-          ? usersWithoutCurrent.map((user, index) => (
-              <Fragment key={user.id}>
-                <HomeUsersAsideListItem
-                  user={user}
-                  blockStyle={BLOCKS[index % BLOCKS.length]}
-                />
-              </Fragment>
-            ))
-          : [...Array(14)].map((item, index) => (
-              <Fragment key={index}>
-                <HomeUsersAsideListItem
-                  isLoading={true}
-                  blockStyle={BLOCKS[index % BLOCKS.length]}
-                />
-              </Fragment>
-            ))}
-      </ul>
-      <div className="duckegg-block h-4 shrink-0" />
-    </aside>
+    <SimpleBar className="main-container mx-4 mt-3 mb-2 hidden md:flex md:w-3xs lg:w-xs">
+      <aside className="flex flex-col gap-2 px-3 pt-3.5 pb-2.5">
+        <header className="flex shrink-0 items-center justify-center gap-2">
+          <div className="yellow-block flex-1 pt-1 pb-0.5">
+            <h2 className="font-chiikawa text-shadow-wrap px-2 pt-2 pb-1 text-center">
+              People you may know
+            </h2>
+          </div>
+        </header>
+        <div className="duckegg-block h-4 shrink-0" />
+        <ul className="flex flex-1 flex-col gap-2">
+          {!isLoading
+            ? usersWithoutCurrent.map((user, index) => (
+                <Fragment key={user.id}>
+                  <HomeUsersAsideListItem
+                    user={user}
+                    blockStyle={BLOCKS[index % BLOCKS.length]}
+                  />
+                </Fragment>
+              ))
+            : [...Array(14)].map((item, index) => (
+                <Fragment key={index}>
+                  <HomeUsersAsideListItem
+                    isLoading={true}
+                    blockStyle={BLOCKS[index % BLOCKS.length]}
+                  />
+                </Fragment>
+              ))}
+        </ul>
+        <div className="duckegg-block h-4 shrink-0" />
+      </aside>
+    </SimpleBar>
   );
 };
 
