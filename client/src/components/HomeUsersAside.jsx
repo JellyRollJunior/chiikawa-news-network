5fa16b38b93cf00d038b873cd174520e3d711bf0
Brandon Lin <brandonlin9716@gmail.com>
2025-10-16 13:46:05 -0700

    Decrease width of userAside on tablet

--- client/src/components/HomeUsersAside.jsx
+++ client/src/components/HomeUsersAside.jsx
@@ -13,7 +13,7 @@ const HomeUsersAside = () => {
     : [];
 
   return (
-    <SimpleBar className="main-container mx-4 mt-3 mb-2 hidden md:flex md:w-3xs lg:w-xs">
+    <SimpleBar className="main-container mr-4 mt-3 mb-2 hidden md:flex md:w-[10rem] lg:w-xs">
       <aside className="flex flex-col gap-2 px-3 pt-3.5 pb-2.5">
         <header className="flex shrink-0 items-center justify-center gap-2">
           <div className="yellow-block flex-1 pt-1 pb-0.5">
--- client/src/components/HomeUsersAsideListItem.jsx
+++ client/src/components/HomeUsersAsideListItem.jsx
@@ -4,17 +4,19 @@ import { LoadingElement } from './LoadingElement.jsx';
 
 const HomeUsersAsideListItem = ({ blockStyle, user, isLoading = false }) => {
   return !isLoading ? (
-    <li className="flex gap-2">
+    <li className="flex gap-2 md:flex-col lg:flex-row">
       <div className={`px-2 pt-2 pb-1.5 ${blockStyle}`}>
         <Avatar
-          className="size-[52px] border-1 border-yellow-500"
+          className="mx-auto size-[52px] border-1 border-yellow-500"
           avatar={user.avatar}
           size={1.75}
           secondaryStyling={true}
         />
       </div>
-      <div className={`flex flex-1 items-center px-2 py-2 ${blockStyle} `}>
-        <h3 className="ml-2 font-semibold">
+      <div
+        className={`flex flex-1 items-center px-2 py-2 ${blockStyle} justify-center lg:justify-start`}
+      >
+        <h3 className="font-semibold lg:ml-2">
           <Link
             className="hover:text-amber-900 hover:underline"
             to={`/users/${user.id}`}
