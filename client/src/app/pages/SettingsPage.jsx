90918551ff4ce0280e5c4b2d70dd60fdf00233fe
Brandon Lin <brandonlin9716@gmail.com>
2026-03-12 19:59:43 -0700

    Move layout logic from Settings to SettingsPage

--- client/src/app/pages/SettingsPage.jsx
+++ client/src/app/pages/SettingsPage.jsx
@@ -3,7 +3,9 @@ import { Settings } from '@/features/users/components/Settings.jsx';
 const SettingsPage = () => {
   return (
     <div className="flex h-full justify-center">
-      <Settings />
+      <div className="main-container mx-4 mt-3 mb-2 w-full max-w-lg">
+        <Settings />
+      </div>
     </div>
   );
 };
--- client/src/features/users/components/Settings.jsx
+++ client/src/features/users/components/Settings.jsx
@@ -10,30 +10,28 @@ const Settings = () => {
   const { logout } = useLogout();
 
   return (
-    <div className="main-container mx-4 mt-3 mb-2 w-full max-w-lg">
-      <SimpleBar className="h-full">
-        <div className="relative px-3 pt-3 pb-2.5 md:pt-3.5">
-          <header className="yellow-block flex justify-center pt-1 pb-0.5">
-            <LogoTitle className="text-lg md:text-2xl" text="Edit Settings" />
-          </header>
-          <section className="mt-2">
-            <SettingsEditAvatar />
-          </section>
-          <section className="mt-2">
-            <SettingsEditBio />
-          </section>
-          <div className="yellow-block mt-2 ml-auto w-fit px-3 py-3.5">
-            <button className="pink-button px-7 py-1" onClick={logout}>
-              Logout
-            </button>
-          </div>
-          <img
-            className="drop-shadow-pink-outline absolute bottom-[6px] left-0 w-[100px]"
-            src={trioScouting}
-          />
+    <SimpleBar className="h-full">
+      <div className="relative px-3 pt-3 pb-2.5 md:pt-3.5">
+        <header className="yellow-block flex justify-center pt-1 pb-0.5">
+          <LogoTitle className="text-lg md:text-2xl" text="Edit Settings" />
+        </header>
+        <section className="mt-2">
+          <SettingsEditAvatar />
+        </section>
+        <section className="mt-2">
+          <SettingsEditBio />
+        </section>
+        <div className="yellow-block mt-2 ml-auto w-fit px-3 py-3.5">
+          <button className="pink-button px-7 py-1" onClick={logout}>
+            Logout
+          </button>
         </div>
-      </SimpleBar>
-    </div>
+        <img
+          className="drop-shadow-pink-outline absolute bottom-[6px] left-0 w-[100px]"
+          src={trioScouting}
+        />
+      </div>
+    </SimpleBar>
   );
 };
 
